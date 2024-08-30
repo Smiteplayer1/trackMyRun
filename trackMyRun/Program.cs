1a4bd2a74dacc0eacffb920c1b2eff9fe15e2b6a
Smiteplayer1 <108710295+Smiteplayer1@users.noreply.github.com>
2024-08-30 08:03:15 -0400

    DBContext and nuget packages installed, Need to scaffold next

--- trackMyRun/Program.cs
+++ trackMyRun/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace trackMyRun
 {
     public class Program
@@ -8,7 +10,11 @@ namespace trackMyRun
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            string connString = builder.Configuration.GetConnectionString("TrackMyRunDB" ?? "");
 
+            builder.Services.AddDbContext<trackMyRunDbContext>(
+                options => options.UseMySql(connString, ServerVersion.AutoDetect(connString))
+                );
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
